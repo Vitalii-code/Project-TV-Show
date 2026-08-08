1337ac1efa4fb2d1ac24038f867090b78bcc73e5
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-08 12:13:26 +0100

    Add parsing from the list

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+
+  const grid = document.getElementById("grid");
+  const template = document.getElementById("episode-card");
+
+  for (const episode of allEpisodes) {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector(".title").textContent =
+      episode.name +
+      " - S" +
+      String(episode.season).padStart(2, "0") +
+      "E" +
+      String(episode.number).padStart(2, "0");
+    clone.querySelector(".thumb").src = episode.image.medium;
+
+    clone.querySelector(".description").innerHTML = episode.summary;
+
+    grid.appendChild(clone);
+  }
+
   makePageForEpisodes(allEpisodes);
 }
 
